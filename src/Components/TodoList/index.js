621a6dd131179ded0c130060c787c4634b7629af
javascriptiser <woronetskianton@gmail.com>
2021-02-06 17:50:52 +0300

    TodoListForm (formik+yup)

--- src/Components/TodoList/index.js
+++ src/Components/TodoList/index.js
@@ -1,10 +1,14 @@
+import style from './style.module.css'
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo, removeLastTodo } from "./slice";
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 
 export const TodoList = () => {
     const todoList = useSelector(state => state.todoReducer.todoList);
-
     return <div>
         <TodoListForm />
         <Todo
@@ -25,16 +29,60 @@ const Todo = ({ todoList }) => {
 };
 
 
+const validationShema = Yup.object({
+    title: Yup.string().required('Required').max(20, 'Max 20 characters'),
+    description: Yup.string().required('Required').max(20, 'Max 20 characters'),
+})
 const TodoListForm = () => {
     const dispatch = useDispatch();
-    const addButtonClickHandler = () => {
-        dispatch(addTodo({ id: Date.now().toLocaleString(), title: prompt() }));
-    }
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: ''
+        },
+        validationSchema: validationShema,
+        onSubmit(values) {
+            dispatch(addTodo({
+                id: Date.now().toLocaleString(),
+                title: values.title,
+                description: values.description
+            }))
+        }
+    })
     const deleteButtonClickHandler = () => {
         dispatch(removeLastTodo());
     }
-    return <div>
-        <button onClick={addButtonClickHandler}>ADD</button>
-        <button onClick={deleteButtonClickHandler}>Delete last</button>
-    </div>
+    return <form onSubmit={formik.handleSubmit} className={style.formWrapper}>
+        <TextField
+            fullWidth
+            id='title'
+            name='title'
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            error={formik.touched.title && formik.errors.title}
+            helperText={formik.touched.title && formik.errors.title}
+        />
+        <TextField
+            fullWidth
+            id='description'
+            name='description'
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            error={formik.touched.description && Boolean(formik.errors.description)}
+            helperText={formik.touched.description && formik.errors.description}
+        />
+        <Button
+            type={'submit'}
+            fullWidth
+            variant="contained"
+            color="primary">ADD
+        </Button>
+        <Button
+            onClick={deleteButtonClickHandler}
+            type={'button'}
+            fullWidth
+            variant="contained"
+            color="secondary">Delete last
+        </Button>
+    </form>
 };
